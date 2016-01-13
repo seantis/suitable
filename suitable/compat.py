5de029961c9a7783f8f520bd6ccc26c03a652149
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-01-13 23:25:04 +0100

    Adds coverage support

--- suitable/compat.py
+++ suitable/compat.py
@@ -4,8 +4,8 @@ PYTHON_3 = sys.version_info[0] == 3
 
 
 if PYTHON_3:
-    string_types = (str, )
-    text_type = str
+    string_types = (str, )  # pragma: no cover
+    text_type = str  # pragma: no cover
 else:
     string_types = (basestring, )  # noqa
     text_type = unicode  # noqa
